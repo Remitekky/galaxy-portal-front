01a367a5ef017e5c76940f77bae25376f66524fb
Remitekky <remitekky@hotmail.fr>
2022-10-26 23:09:54 +0200

    Using the metamask's API to see if a wallet is installed on th euser's browser. See if we're authorized to acces an account of user's wallet to memorize it into the state. If not, give the possibility to the user to connect his account by request the JSONRPC method 'eth_requestAccounts' : give access to the user's accounts and memorize the first one into the state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // import { ethers } from "ethers";
 import './App.css';
 
 export default function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  useEffect(() => {
+    const init = async () => {
+      const account = await findMetaMaskAccount();
+      if (account !== null) {
+        setCurrentAccount(account);
+      }
+    }
+
+    init();
+  }, []);
 
   // When logged /w metamask, an etehreum object is injected into window
   const getEthereumObject = () => window.ethereum;
-  
-  const star = () => {
-    
-  }
 
-  useEffect(() => {
-    if (getEthereumObject) {
-      console.log("Your are logged with your metamask wallet address", getEthereumObject());
-    } else {
-      console.log("Please connect your metamask wallet");
+  /*
+   * This function returns the first linked account found.
+   * If there is no account linked, it will return null.
+   */
+  const findMetaMaskAccount = async () => {
+    try {
+      const ethereum = getEthereumObject();
+
+      // 1. Check for ethereum object into window
+      if (!ethereum) {
+        console.log("Make sure you have Metamask installed 🦊");
+        return null;
+      }
+
+      console.log("We have an etehreum object");
+      // 2. Trying to see if we're authorized to access any of the accounts in the user's wallet
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        return account;
+      } else {
+        console.error("No authorized account found");
+        return null;
+      }
+
+    } catch (error) {
+      console.error(error);
+      return null;
     }
-  }, []);
+  };
+
+  /**
+   * Function to connect the user with her wallet
+   * We asking metamask acces of the user's wallet to interact with our smart contract
+   */
+  const connectMetamask = async () => {
+    const ethereum = getEthereumObject();
+
+      // 1. Check for ethereum object into window
+    if (!ethereum) {
+      console.log("Please install Metamask 🦊");
+      return null;
+    }
+
+    // 2. Asking Metamask to give access to the user's wallet
+    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    setCurrentAccount(accounts[0]);
+  };
+
+  const star = () => {
+
+  };
 
   return (
     <div className="mainContainer">
@@ -34,6 +89,12 @@ export default function App() {
         <button className="starButton" onClick={star}>
           Throw me a star ⭐️
         </button>
+
+        {!currentAccount && (
+          <button className="starButton" onClick={connectMetamask}>
+            Connect with Metamask 🦊
+          </button>
+        )}
       </div>
     </div>
   );
